8389d7e4eca02b588dc0202a82805a30217b74df
Will Waldron <wvw0330@uah.edu>
2017-09-06 16:22:01 -0500

    Added simple photometry calc

--- hsttools/photometry.py
+++ hsttools/photometry.py
@@ -12,6 +12,15 @@ from __future__ import division
 # Numerical Imports
 import numpy as np
 
+# Astro Imports
+from astropy             import units as u
+from astropy.wcs         import WCS
+from astropy.io          import fits
+from astropy.coordinates import SkyCoord
+from photutils           import CircularAperture,       CircularAnnulus
+from photutils           import SkyCircularAperture, SkyCircularAnnulus
+from photutils           import aperture_photometry
+
 
 # --- Magnitude ---------------------------------------------------------------
 def magnitude(flux, zeroPoint, distToSrc=10, galExt=0, apCor=0):
@@ -39,3 +48,41 @@ def zeropoint(photFlam, photPlam):
     abMagZpt = -2.5*np.log10(photFlam) - 21.1 - 5*np.log10(photPlam) + 18.692
     
     return stMagZPt, abMagZpt
+
+
+# --- Simple Aperature Photometery --------------------------------------------
+def simpleapphot(fileName,pos,r,rI,rO,frame='image'):
+    '''
+    '''
+    
+    # Create Aperture
+    frame = frame.lower()
+    if frame == 'image':
+        
+        # Create the Aperatures
+        cirAps = CircularAperture(pos, r)
+        annAps = CircularAnnulus( pos, rI, rO)
+        
+    elif frame == 'fk5':
+        
+        # Create Sky Apertures
+        pos = SkyCoord(frame=frame, ra=pos[:,0]*u.deg, dec=pos[:,1]*u.deg)
+        cirAps = SkyCircularAperture(pos, r *u.arcsec)
+        annAps = SkyCircularAnnulus( pos, rI*u.arcsec, rO*u.arcsec)
+        
+    else:
+        raise ValueError('Unsupported coordinate system.')
+    
+    # Load in the files and do photometry
+    hdu        = fits.open(fileName)
+    cirPhotTab = aperture_photometry(hdu,cirAps)
+    annPhotTab = aperture_photometry(hdu,annAps )
+    if frame == 'fk5':
+        cirAps = cirAps.to_pixel(WCS(fobj=hdu))
+        annAps = annAps.to_pixel(WCS(fobj=hdu))
+    hdu.close()
+    
+    # Get Photometry as ndarray
+    phot = cirPhotTab['aperture_sum'].data - \
+           (cirAps.area()/annAps.area())*annPhotTab['aperture_sum'].data
+    return phot
