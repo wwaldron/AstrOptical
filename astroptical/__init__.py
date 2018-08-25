cad368532f32b6aa079efdad3a0a806c0825ab57
Will Waldron <wvw0330@uah.edu>
2018-08-25 18:05:40 -0500

    Explicit subpackage imports

--- astroptical/__init__.py
+++ astroptical/__init__.py
@@ -9,13 +9,13 @@ __version__ = pkg_resources.get_distribution('astroptical').version
 del pkg_resources
 
 # Local Impots
-from . import cosmicray
-from . import fitsutil
-from . import photometry
-from . import sextractor
-from . import ssp
-from . import reddening
-from . import metrics
+from astroptical import cosmicray
+from astroptical import fitsutil
+from astroptical import photometry
+from astroptical import sextractor
+from astroptical import ssp
+from astroptical import reddening
+from astroptical import metrics
 
 # Astropy Imports
 #from astropy.io import fits as fitsio  # Not sure whether ethical or good practice
