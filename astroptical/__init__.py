d331ac6233cb6e0ffebd0d34193fc825a961713f
Will Waldron <wvw0330@uah.edu>
2018-09-06 13:30:51 -0500

    Updated init order

--- astroptical/__init__.py
+++ astroptical/__init__.py
@@ -10,13 +10,13 @@ __version__ = pkg_resources.get_distribution('astroptical').version
 del pkg_resources
 
 # Local Impots
+from astroptical import metrics
 from astroptical import cosmicray
 from astroptical import fitsutil
 from astroptical import photometry
 from astroptical import sextractor
 from astroptical import ssp
 from astroptical import reddening
-from astroptical import metrics
 
 # Astropy Imports
 #from astropy.io import fits as fitsio  # Not sure whether ethical or good practice
